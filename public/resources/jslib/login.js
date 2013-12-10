9eb71186eb0ff94249e7b6e963bcc95ccb0ede4c
bulibuliking <bulibuliking@163.com>
2013-12-10 16:38:18 +0800

    修改登录窗口的JS代码

--- public/resources/jslib/login.js
+++ public/resources/jslib/login.js
@@ -1,8 +1,8 @@
 // JavaScript Document
 var login_form;
 $(function(){
-	login_form = $('#login_form').form({
-		url:'/login.json',
+	login_form = $('#login_form').form('submit', {
+		url:'/login',
 		success:function(data){
 			var data = $.parseJSON(data);
 			showMsgBox(data.msg,'提示信息');
